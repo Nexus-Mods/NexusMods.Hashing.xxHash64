bccb8caa423c0d6dc521affe1cc7fd045869cb29
Timothy Baldridge <tbaldridge@gmail.com>
2023-08-15 13:56:22 -0600

    Add the code and the tests, disable rate limiting related code for now

---
+++ tests/NexusMods.Hashing.xxHash64.Tests/StringExtensionTest.cs
@@ -0,0 +1,13 @@
+using FluentAssertions;
+
+namespace NexusMods.Hashing.xxHash64.Tests;
+
+public class StringExtensionTest
+{
+    [Fact]
+    public void CanConvertReadOnlySpans()
+    {
+        var arr = new byte[] { 0xDE, 0xAD, 0xBE, 0xEF };
+        ((ReadOnlySpan<byte>)arr.AsSpan()).ToHex().Should().Be("DEADBEEF");
+    }
+}
